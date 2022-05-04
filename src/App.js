79df6ce165805e625ebbb82c974f33b0da8db06b
Enrique Barco <enrique.barco96@hotmail.com>
2022-05-04 14:25:50 -0400

    created algo to succesfuly update ladder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { formatData } from "./utils";
+import uniqid from "uniqid";
+import _ from "lodash";
 
 function App() {
   const [currencies, setCurrencies] = useState([]);
@@ -8,6 +10,8 @@ function App() {
   const [granularity, setGranularity] = useState(86400);
   const [price, setPrice] = useState("0.00")
   const [historicalData, setHistoricalData] = useState({});
+  const [asks, setAsks] = useState({});
+  const [bids, setBids] = useState({});
   const ws = useRef(null);
 
   let first = useRef(false);
@@ -16,6 +20,7 @@ function App() {
   useEffect(() => {
     ws.current = new WebSocket("wss://ws-feed.pro.coinbase.com");
 
+    // API call to get a list of all currency pairs
       axios.get(`${url}/products`)
         .then((res) => {
           const mainPairsArr = ["BTC-USD", "ETH-USD", "LTC-USD", "BCH-USD"]
@@ -36,10 +41,12 @@ function App() {
   }, []);
 
   useEffect(() => {
+    //ensuring websocket connection is established
     if(!first.current) {
       return;
     }
 
+    //subscribe to websocket channels: ticker used for price and level2 used for ladder
     let msg = {
       type: "subscribe",
       product_ids: [pair],
@@ -49,27 +56,72 @@ function App() {
     ws.current.send(jsonMsg);
 
     let historicalDataUrl = `${url}/products/${pair}/candles?granularity=${granularity}`;
-
+    
+    // API call to get historical prices, used for charts
     axios.get(historicalDataUrl)
       .then((res) => setHistoricalData(formatData(res.data)))
       .catch((err) => console.log(err));
 
+      
+    }, [pair]);
+    
+    useEffect(() => {
+    //handling all the responses from the websocket
     ws.current.onmessage = (e) => {
       let data = JSON.parse(e.data);
-      console.log(data);
+
+      //handle ticker channel response for pricing
       if(data.type === "ticker") {
         if(data.product_id === pair) {
-          let price = data.price.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+          let price = data.price.replace(/\B(?=(\d{3})+(?!\d))/g, ","); //regex to add commas for user readability
           setPrice(price);
         }
       }
 
-      if(data.type === ("snapshot" || "l2update")) {
-        console.log(data);
+      //handle initial level2 response
+      if(data.type === "snapshot") {
+
+        let asksMap = new Map();
+        let bidsMap = new Map();
+
+        //only saving initial twenty bid/asks to prevent stack overflow and optimize speed
+        for(let i = 0; i < 20; i++) {
+          asksMap.set(data.asks[i][0], parseFloat(data.asks[i][1]));
+          bidsMap.set(data.bids[i][0], parseFloat(data.bids[i][1]));
+        }
+
+        setAsks(asksMap);
+        setBids(bidsMap);
+
+        return;
+      }
+      
+      //handling consequent level2 channel responses which will be used to update ladder created with the initial response
+      if(data.type === "l2update") {
+        
+        
+        data.changes.forEach(change => {
+          const [action, price, amount] = change;
+          const parsed = parseFloat(amount);
+
+
+          if(action === "sell") {
+            if(parsed) {
+              setAsks((prevAsks) => prevAsks.set(price, parsed));
+            } else {
+              // setAsks((prevAsks) => prevAsks.delete(price));
+            }
+          } else {
+            // if(parsed) {
+            //   setBids((prevBids) => prevBids[price] = parsed);
+            // } else {
+            //   setBids((prevBids) => delete prevBids[price]);
+            // }
+          };
+        });
       }
 
     };
-
   }, [pair]);
 
   const handleSelect = (e) => {
@@ -83,9 +135,9 @@ function App() {
     ws.current.send(unsubscribe);
 
     setPair(e.target.value);
-  }
+  };
 
-  return (
+   return (
     <div className="App">
       
         <select name="currency" value={pair} onChange={handleSelect}>
@@ -97,7 +149,19 @@ function App() {
             )
           })}
         </select>
+
         <h1>Price: ${price}</h1>
+        
+        {
+          (_.isEmpty(asks) || _.isEmpty(bids)) ?
+            <h2>establishing connection...</h2>
+          :
+            [...asks.entries()].map((value, key) => {
+              return (
+                <li key={uniqid()}>Price: ${value} || Size: {key}</li>
+            )
+            })
+        }
     </div>
   );
 }
